233c2f36dff23935c8a9a999b0d9d2bf005b112a
Ammar Bandukwala <ammar@ammar.io>
2024-09-18 14:23:07 -0500

    Fix brew formula to use tarball and set version

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ SHELL := /bin/bash
 build:
 	set -e
 	mkdir -p bin
-	VERSION=$$(git describe --tags --always --dirty || echo "dev")
+	if [ -z "$$VERSION" ]; then \
+		VERSION=$$(git describe --tags --always --dirty || echo "dev"); \
+	fi
 	echo "Building version $${VERSION}"
 	go build -ldflags "-X main.Version=$${VERSION}" -o bin/aicommit ./cmd/aicommit
--- brew/aicommit.rb
+++ brew/aicommit.rb
@@ -1,16 +1,18 @@
 class Aicommit < Formula
     desc "AI-powered commit message generator"
     homepage "https://github.com/coder/aicommit"
-    url "https://github.com/coder/aicommit.git", revision: "HEAD"
-    version "0.0.0" # This will be overridden by the git describe in the Makefile
+    version "0.6.2"
+    url "https://github.com/coder/aicommit/archive/refs/tags/v#{version}.tar.gz"
+    sha256 "04a980875e97aebc0f7bbeaa183aa5542b18c6e009505babbd494fe0fe62b18e"
     license "CC0-1.0"
 
     depends_on "go" => "1.21"
     depends_on "make" => :build
 
     def install
-        system "git", "pull"
-        system "make", "build"
+        version = "v#{version}"
+        ENV["VERSION"] = version
+        system "make", "build", "VERSION=#{version}"
         bin.install "bin/aicommit"
     end
 
